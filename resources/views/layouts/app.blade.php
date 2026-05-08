8ff228019267e1c462e688c1c25f96fa3fdd2fbc
stevearauld <stevearauld@gmail.com>
2026-05-08 21:52:26 +0100

    init6

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
 
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="trustpilot-one-time-domain-verification-id" content="b6b745e2-10f2-49bb-b60d-e055067b876e"/>
         <title>
             {{ trim(View::yieldContent('title') . ' | ' . config('app.name')) }}
         </title>
